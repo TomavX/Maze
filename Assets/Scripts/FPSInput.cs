540c856ae84f723ac97e827eab94d6bd6a9e8f22
TomavX <acwokup@gmail.com>
2017-03-13 08:42:20 +0100

    Add FPSInput.cs

---
+++ Assets/Scripts/FPSInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInput : MonoBehaviour {
+    public float speed = 6f;
+
+    private CharacterController _characterController;
+
+	// Use this for initialization
+	void Start () {
+        _characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed),speed) * Time.deltaTime;
+        _characterController.Move(transform.TransformDirection(movement));
+
+ //       transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+	}
+}
