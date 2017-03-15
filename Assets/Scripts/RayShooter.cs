d8b96f651f5d782b77ac49034f3957102eda9951
TomavX <acwokup@gmail.com>
2017-03-15 06:23:35 +0100

    Add aiming indicator

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -8,6 +8,8 @@ public class RayShooter : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         _camera = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
@@ -23,6 +25,14 @@ public class RayShooter : MonoBehaviour {
             }
         }
 	}
+
+    private void OnGUI()
+    {
+        int size = 12;
+        float posX = _camera.pixelWidth / 2 - size / 4;
+        float posY = _camera.pixelHeight / 2 - size / 2;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
 
     private IEnumerator SphereIndicator(Vector3 pos)
     {
