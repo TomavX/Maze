a99fbd2872c3ac97753a8fdafbca430110505ff6
TomavX <acwokup@gmail.com>
2017-03-15 07:35:52 +0100

    Add enemy spawn from prefab ability

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+    [SerializeField]
+    private GameObject enemyPrefab;
+    private GameObject _enemy;
+
+	// Update is called once per frame
+	void Update () {
+		if(_enemy == null)
+        {
+            _enemy = Instantiate(enemyPrefab) as GameObject;
+            _enemy.transform.position = new Vector3(0, 1, 0);
+            _enemy.transform.Rotate(0, Random.Range(0, 360), 0);
+        }
+	}
+}
